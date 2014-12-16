7561dee624dd40137946205dec70ea4bf247d2d5
jesusmariocalleja <jesus.mario.calleja@gmail.com>
2014-12-16 12:07:59 +0100

    classificador.py

--- classificador.py
+++ classificador.py
@@ -51,32 +51,32 @@ def getImages(_fileName):
 
 
 def classify():
-    cont = 1 # Aquestes variables (cont i length) són simplement pel text que es mostra a la consola
+    cont = 1  # Aquestes variables (cont i length) són simplement pel text que es mostra a la consola.
     length = len(imagesList)
 
     oFile = open("classified.csv", 'wb')
     wr = csv.writer(oFile)
 
-    wr.writerow(["document_id event_type"]) # Escribim la primera línia
+    wr.writerow(["document_id event_type"])  # Escribim la primera línia
 
     for image in imagesList:  # Per cada imatge
         print ("Classifying image " + image.document_id + " (" + str(cont) + " of " + str(length) + ")")
         for imageTag in image.tags_list:  # Agafem cadascún dels seus tags
             for event in eventsTagsList:
                 match = 0
-                for eventTag in event.tags_list:  # I el busquem a la llista de events-tags de l'event
-                    if eventTag == imageTag: # Si troba el tag en aquell event, el comptador de l'event +1
+                for eventTag in event.tags_list:  # I el busquem a la llista de events-tags de l'event.
+                    if eventTag == imageTag:  # Si troba el tag en aquell event, el comptador de l'event +1.
                         match += 1
                         break
                 event.match_counter += match
         winner_counter = 0
         winner_event = ""
-        for event in eventsTagsList: # Aquí simplement mirem l'event amb el comptador més alt i li assignem a la imatge
+        for event in eventsTagsList:  # Aquí simplement mirem l'event amb el comptador més alt i li assignem a la imatge.
             if event.match_counter > winner_counter:
                 winner_counter = event.match_counter
                 winner_event = event.name
-            event.match_counter = 0 # Un cop analitzat el comptador de l'event, el reiniciem per la següent imatge
-        if winner_counter == 0: # Si una imatge no té cap event assignat, se li posa non_event
+            event.match_counter = 0  # Un cop analitzat el comptador de l'event, el reiniciem per la següent imatge.
+        if winner_counter == 0:  # Si una imatge no té cap event assignat, se li posa non_event.
             image.event_type = "non_event"
         else:
             image.event_type = winner_event
@@ -89,11 +89,17 @@ def classify():
 #### MAIN PROGRAM ####
 ######################
 
-print ("Getting events and related tags")
-eventsTagsList = getEventsTags("TF_IDF.csv")  # Llegim la llista on tenim tots els tags per cada event
+#FILES VARS
+eventTagsFileName = "TF_IDF.csv"
+evaluableImageTagsFileName = "id_tags.csv"
 
-print ("Getting images to classify")
-imagesList = getImages("id_tags.csv")  # Llegim la llista amb les imatges i els tags que ens passa el descriptor
 
-print ("Starting to classify")
+print("Getting events and related tags")
+eventsTagsList = getEventsTags(eventTagsFileName)  # Llegim la llista on tenim tots els tags per cada event.
+
+print("Getting images to classify")
+imagesList = getImages(evaluableImageTagsFileName)  # Llegim la llista amb les imatges i els tags que ens passa el descriptor.
+
+print("Starting to classify")
 classify()
+
